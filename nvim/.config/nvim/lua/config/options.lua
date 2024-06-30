c37fa1a40ed22948950a14fc9f2e5e7f22245d37
Bhushan Khanale <bkhanale@gmail.com>
2024-06-30 22:02:31 +0530

    Update files for neovim

---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,35 @@
+-- Line numbers
+vim.opt.number = true
+
+-- Indentation
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- Search
+vim.opt.ignorecase = true
+vim.opt.hlsearch = false
+
+-- Splitting
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Mouse
+vim.opt.mouse = 'a'
+
+
+-- Undo
+vim.opt.undofile = true
+
+-- Updatetime
+vim.opt.updatetime = 250
+
+-- Sign column
+vim.opt.signcolumn = 'yes'
+
+-- 24-bit RBG color
+vim.opt.termguicolors = true
+
+-- Line length
+vim.opt.colorcolumn = '120'
